1b16f57d61a414ed9af6da8d61c08ccdb5d5fb40
Haleeman100 <s.m.sabo2@newcastle.ac.uk>
2022-06-12 01:08:24 +0100

    change picture and added header contents

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,23 @@
 import React, {useContext}from 'react'
-import link from 'next/link';
+import Link from 'next/link';
 
-const Categories = [{name: 'React', slug: 'react'},{name: 'Web Development', slug: 'web-dev'} ]
+const Categories = [{ name: ' React', slug: 'react'}, 
+                    { name: 'Web Development', slug: 'web-dev'} ]
 
 const Header = () => {
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className='border-b w-full inline-block border-blue-400 py-8'>
             <div className='md:float-left block'>
-                <Link href="/">
-                    <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS>
+              <Link href='/'>
+                    <span className="cursor-pointer font-bold text-4xl text-white">
                         BLOG DOC
                     </span>
-                
-                </Link>
+               </Link>
                 <div className='hidden md:float-left md:contents'>
 
                     {Categories.map((cattegory) => (
-                        <Link> key={cattegory.slug} href={'/cattegory/${cattegory.slug}'}
+                        <Link key={cattegory.slug} href={'/cattegory/${cattegory.slug}'}>
                         <span className='md:float-right mt2 align-middle text-white ml=4 font-semibold cursor-pointer'>
                             {cattegory.name}
                         </span>
---
+++ components/Layout.jsx
@@ -0,0 +1,14 @@
+import React, { Children } from 'react'
+import { Header } from './'
+
+const Layout = ({Children}) => {
+  return (
+    <>
+        <Header />
+        {Children}
+
+    </>
+  )
+}
+
+export default Layout
